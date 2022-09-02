8f43d10c9e6208e75d7dd8d838e2d6b3763dec0d
Clara Lima <86741485+claralima1@users.noreply.github.com>
2022-09-02 12:43:55 -0300

    atualização

--- Estudo_Jogo_Paint.py
+++ Estudo_Jogo_Paint.py
@@ -15,21 +15,21 @@ from turtle import * #para importar a biblioteca Turtle
 from freegames import vector # para importar para importar a biblioteca vector
 
 
-def line(start, end): # método para desenhar uma linha
+def line(start, end): # método para determinar a posição inicial e fianl da caneta e desenhar uma linha
     "Draw line from start to end." 
     up() # para puxar a caneta para cima sem desenhar ao se mover
     goto(start.x, start.y) #método (par de cordenadas) (X- um número ou um par/vetor de números | y – um número ouNone)
     down() # para puxar a caneta para baixo, desenhando ao se mover
     goto(end.x, end.y)
 
-def square(start, end): # método para desenhar um quadrado
+def square(start, end): # método para determinar a posição inicial e final da caneta
     "Draw square from start to end."
     up()
     goto(start.x, start.y)
     down()
     begin_fill() #Para ser chamado antes de desenhar uma forma a ser preenchida.
 
-    for count in range(4): # função 
+    for count in range(4): # função para desenhar um quadrado
         forward(end.x - start.x)
         left(90)
 
@@ -93,17 +93,17 @@ onscreenclick(tap)
 
 listen()
 
-onkey(undo, 'u')
-onkey(lambda: color('black'), 'K')
-onkey(lambda: color('white'), 'W')
-onkey(lambda: color('green'), 'G')
-onkey(lambda: color('blue'), 'B')
-onkey(lambda: color('red'), 'R')
-onkey(lambda: color('purple'), 'P')
-onkey(lambda: store('shape', line), 'l')
-onkey(lambda: store('shape', square), 's')
-onkey(lambda: store('shape', circle), 'c')
-onkey(lambda: store('shape', rectangle), 'r')
-onkey(lambda: store('shape', triangle), 't')
+onkey(undo, 'u') #borracha ("controlZ")
+onkey(lambda: color('black'), 'K')#botão para mudar de cor
+onkey(lambda: color('white'), 'W')#botão para mudar de cor
+onkey(lambda: color('green'), 'G')#botão para mudar de cor
+onkey(lambda: color('blue'), 'B')#botão para mudar de cor
+onkey(lambda: color('red'), 'R')#botão para mudar de cor
+onkey(lambda: color('purple'), 'P')#botão para mudar de cor
+onkey(lambda: store('shape', line), 'l')#botão para fazer uma linha
+onkey(lambda: store('shape', square), 's')#botão para fazer um quadrado
+onkey(lambda: store('shape', circle), 'c')#botão para fazer uum circulo
+onkey(lambda: store('shape', rectangle), 'r')#botão para fazer um retângulo
+onkey(lambda: store('shape', triangle), 't')#botão para fazer um triângulo
 done()
 
